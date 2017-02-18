1a23860c3bbb5ce310bc3d436f78737354c2283f
Claire Treyz <treyz@mit.edu>
2017-02-18 10:19:12 -0800

    Add assets. Add spinning wheel to the main game scene

--- Chroma Game/GameViewController.swift
+++ Chroma Game/GameViewController.swift
@@ -1,55 +1,44 @@
-//
-//  GameViewController.swift
-//  Chroma Game
-//
-//  Created by Claire on 2/16/17.
-//  Copyright © 2017 Claire Treyz. All rights reserved.
-//
-
 import UIKit
 import SpriteKit
 import GameplayKit
 
+var SKViewSize: CGSize?
+var SKViewSizeRect: CGRect?
+
 class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    if let view = self.view as! SKView? {
+      SKViewSize = view.bounds.size
+      
+      if let scene = SKScene(fileNamed: "GameScene") as? GameScene {
+        scene.scaleMode = .aspectFill
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
-    }
+        view.presentScene(scene)
+        SKViewSizeRect = getViewSizeRect()
+        scene.sizeColorWheel()
+      }
+      
+      view.ignoresSiblingOrder = true
 
-    override var shouldAutorotate: Bool {
-        return true
     }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+  }
+  
+  override var shouldAutorotate: Bool {
+    return true
+  }
+  
+  override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+    return .landscape
+  }
+  
+  override var prefersStatusBarHidden: Bool {
+    return true
+  }
+  
+  func getViewSizeRect() -> CGRect {
+    return CGRect(x: ((SKViewSize!.width  * 0.5) * -1.0), y: ((SKViewSize!.height * 0.5) * -1.0), width: SKViewSize!.width, height: SKViewSize!.height)
+  }
 }
---
+++ README.md
@@ -0,0 +1,6 @@
+Copyright 2014 Claire Treyz
+
+Re-write of the Chrome Game using Swift and SpriteKit
+
+Old version available in the app store (FREE): https://itunes.apple.com/us/app/the-chroma-game/id896406848?ls=1&mt=8
+
